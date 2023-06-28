a5b81e6e0190f6a584bc4519adbf2bd45bfea0f4
sacud <antunes.duart@gmail.com>
2023-06-28 13:56:20 -0400

    added components

---
+++ vilabispo/components/Card.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
+const Card = ({ post, handleTagClick }) => {
+
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  return (
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+
+        <div className='copy_btn' onClick={handleCopy}>
+         
+        </div>
+      </div>
+
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p
+        className='font-inter text-sm blue_gradient cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        #{post.tag}
+      </p>
+
+    </div>
+  );
+};
+
+export default Card;
