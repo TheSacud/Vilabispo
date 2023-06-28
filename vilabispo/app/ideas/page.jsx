953b50769517382d7019a86c856947accac86bab
sacud <antunes.duart@gmail.com>
2023-06-27 21:06:19 -0400

    connected to db

--- vilabispo/app/ideas/page.jsx
+++ vilabispo/app/ideas/page.jsx
@@ -1,21 +1,37 @@
 "use client";
 
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import Form  from '@components/Form';
 
 
 const Ideas = () => {
+  const router = useRouter();
   const [submit, setSubmit] = useState(false);
-  const [post, setPost] = useState({
-    prompt:'',
-    tag: '',
-  })
+  const [post, setPost] = useState({prompt:'',tag: ''});
 
   const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmit(true);
+    try{
+      const resp = await fetch('/api/prompt/new',{
+        method:'POST',
+        body: JSON.stringify({
+          prompt:post.prompt,
+          tag: post.tag
+        })
+      });
+      if(resp.ok){
+        router.push('/');
+      }
+    }catch(error){
+      console.log(error);
+    }finally {
+      setSubmit(false);
+    }
+  };
 
-  }
   return (
     <Form
       type="Ideas"
@@ -24,7 +40,7 @@ const Ideas = () => {
       submit={submit}
       handleSubmit={createPrompt}
     />
-  )
-}
+  );
+};
 
 export default Ideas;
--- vilabispo/app/page.jsx
+++ vilabispo/app/page.jsx
@@ -19,9 +19,10 @@ const Home = () => {
         <div>
           <p className="desc tex-center">These are questions I ask myself every time I work on this project...
             My family owns about 164 hectares in the Algarve, near Vila do Bispo. The land is owned by 4 people, but they are all willing to sell.
-            For many years we have been trying to sell the land, but it is difficult because it is complicated to get building permits and we have no capital to invest.
+            For many years we have been trying to sell the land, but it is difficult to get building permits and we have no capital to invest.
             Most of the land is only allowed to be farmed, with a few exceptions.
             Having said this, I believe there is something that can be done with the land, we just need ideas and capital to get started. And that is the goal of this project.
+            To farm ideas and maybe get interested people to invest.
           </p>
         </div>
         
